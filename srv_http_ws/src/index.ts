4c87b9398327bec1816368cf2ad523c2f7c3b902
charli <charlyoleg@fabfolk.com>
2019-08-24 00:08:53 +0200

    server base done

--- srv_http_ws/src/index.ts
+++ srv_http_ws/src/index.ts
@@ -14,10 +14,30 @@ server.listen(8005, function () {
    console.log("In your browser, open http://localhost:%s", port);
 });
 
+//////////////////////////////
+// serving files
+//////////////////////////////
+
 app.get('/', function (req: any, res: any) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/js/kiss_socketio.js', function (req: any, res: any) {
+  res.sendFile(__dirname + '/kiss_socketio.js');
+});
+
+app.get('/css/kiss_socketio_style.css', function (req: any, res: any) {
+  res.sendFile(__dirname + '/kiss_socketio_style.css');
+});
+
+//////////////////////////////
+// rest api
+//////////////////////////////
+
+
+//////////////////////////////
+// websocket service
+//////////////////////////////
 io.on('connection', function (socket: any) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data: any) {
